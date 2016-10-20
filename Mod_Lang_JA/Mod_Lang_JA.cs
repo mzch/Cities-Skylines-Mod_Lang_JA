78ad413006cb2c4a659b9ac1f9260f1d98b216bf
Koichi MATSUMOTO <koichi@letlive.com>
2016-10-20 13:54:43 +0900

    パッチ 1.5.2-f3 (Studiums DLC) 対応

--- Mod_Lang_JA/Mod_Lang_JA.cs
+++ Mod_Lang_JA/Mod_Lang_JA.cs
@@ -202,7 +202,7 @@ namespace Mod_Lang_JA
 
 		public string Description
 		{
-			get { return "Japanese Localization v9.1, by volunteers on 2ch.net."; }
+			get { return "Japanese Localization v9.2, by volunteers on 2ch.net."; }
 		}
 	}
 }
