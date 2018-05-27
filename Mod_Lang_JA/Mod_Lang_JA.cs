d9e150a8a3b20876cb0a6dd83d4cc11af2eed8ad
Koichi MATSUMOTO <koichi@letlive.com>
2018-05-27 18:52:17 +0900

    翻訳修正

--- Mod_Lang_JA/Mod_Lang_JA.cs
+++ Mod_Lang_JA/Mod_Lang_JA.cs
@@ -202,7 +202,7 @@ namespace Mod_Lang_JA
 
 		public string Description
 		{
-			get { return "Japanese Localization v15.0, by volunteers on 5ch.net."; }
+			get { return "Japanese Localization v15.1, by volunteers on 5ch.net."; }
 		}
 	}
 }
