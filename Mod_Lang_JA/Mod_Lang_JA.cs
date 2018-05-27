ce3c0cbcc79cf2b832e01201d91afe419d8182c8
Koichi MATSUMOTO <koichi@letlive.com>
2018-05-27 10:29:32 +0900

    パッチ 1.10.0-f3 に対応

--- Mod_Lang_JA/Mod_Lang_JA.cs
+++ Mod_Lang_JA/Mod_Lang_JA.cs
@@ -202,7 +202,7 @@ namespace Mod_Lang_JA
 
 		public string Description
 		{
-			get { return "Japanese Localization v14.0, by volunteers on 2ch.net."; }
+			get { return "Japanese Localization v15.0, by volunteers on 5ch.net."; }
 		}
 	}
 }
