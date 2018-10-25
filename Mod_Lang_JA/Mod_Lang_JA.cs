3febb4c4f77547c18cc4d7be275f069a6a8d69ea
Koichi MATSUMOTO <koichi@letlive.com>
2018-10-26 01:27:00 +0900

    パッチ 1.11.0-f3 (Industries DLC) へ対応

--- Mod_Lang_JA/Mod_Lang_JA.cs
+++ Mod_Lang_JA/Mod_Lang_JA.cs
@@ -202,7 +202,7 @@ namespace Mod_Lang_JA
 
 		public string Description
 		{
-			get { return "Japanese Localization v15.11, by volunteers on 5ch.net."; }
+			get { return "Japanese Localization v16.0, by volunteers on 5ch.net."; }
 		}
 	}
 }
